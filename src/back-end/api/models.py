e4fb8f63db71e6752147640578ecd3e5b9a14f13
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2025-01-24 18:15:45 -0500

    formatted all backend apis

--- src/back-end/api/models.py
+++ src/back-end/api/models.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Dict
 
 class ReportData(BaseModel):
     latitude: float
     longitude: float
     disaster_name: str
     time: int
+
+class Categories(BaseModel):
+    size: int
+    data: List[Dict[str, str]]
