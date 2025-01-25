b561154899e2230587f90abe96db111017170621
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2025-01-24 19:59:32 -0500

    built out api end points!!

--- src/back-end/api/models.py
+++ src/back-end/api/models.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional, List, Dict
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(v)
 
 class ReportData(BaseModel):
     latitude: float
@@ -9,4 +21,5 @@ class ReportData(BaseModel):
 
 class Categories(BaseModel):
     size: int
-    data: List[Dict[str, str]]
+    data: List[str]
+    name: str
