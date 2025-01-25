809353610de388047246ad606de56bc01fdbd34a
Elijah-Cosby <elijahcosby05@outlook.com>
2025-01-24 19:32:21 -0500

    adding the model and updating the versions of packages

--- src/back-end/api/main.py
+++ src/back-end/api/main.py
@@ -13,22 +13,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#Create a POST endpoint
-@app.post("/submit-disaster/")
+#Submit a new report
+@app.post("/submit")
 async def submit_disaster(data: ReportData):
     try:
         latitude = data.latitude
         longitude = data.longitude
         disaster_name = data.disaster_name
         time = data.time
+        #check if category exists in mongo if not, fix that
+        #else, continue
+        #add to mongo db
         return {
             {
-                "latitude": latitude,
-                "longitude": longitude,
-                "disaster_name": disaster_name,
-                "time": time
+               "status": "ok"
             }
         }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.get("/all")
+async def get_all():
+    try:
+        #get all documents from mongodb
+        #get take out types document
+
+        #return the results
+        print("change this")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.get("/categories")
+async def get_categories():
+    try:
+        #find document with specific id for the categories in mongo
+        # return that document
+        #size +=1 
+        print("change this")
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
